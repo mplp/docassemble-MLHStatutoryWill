75eaa993b7332c8fabacb4ce2ae5c636110222c1
Emily Miller <ekressmiller@gmail.com>
2025-12-12 15:23:17 -0500

    update embedded survey

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MLHStatutoryWill',
-      version='1.0.2',
+      version='1.0.3',
       description=('A docassemble extension.'),
-      long_description='# docassemble.MichiganStatutoryWill\r\n\r\nA tool to create a statutory will in Michigan. \r\n\r\n## Authors:\r\n\r\nMichigan Legal Help\r\nLemma Legal\r\n\r\n## Changelog:\r\n* 10/28/25  1.0.2 add link in instructions and improve screenreader tag in will',
+      long_description='# docassemble.MichiganStatutoryWill\r\n\r\nA tool to create a statutory will in Michigan. \r\n\r\n## Authors:\r\n\r\nMichigan Legal Help\r\nLemma Legal\r\n\r\n## Changelog:\r\n* 12/12/25  1.0.3 update embedded survey\r\n* 10/28/25  1.0.2 add link in instructions and improve screenreader tag in will',
       long_description_content_type='text/markdown',
       author='Emily Miller',
       author_email='ekressmiller@lsscm.org',
